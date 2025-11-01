69d3bcd63f6114029c88ba0a3d284ef9cb237cd8
Sebastián Contreras Lugo <seb4stian53@gmail.com>
2025-11-01 13:50:08 -0600

    Capa model
    creacion de las clases de la capa model
    wazaaaaaaa

---
+++ ProyectoTicketDeTurno/src/main/java/com/fabroysebadev/proyectoticketdeturno/model/Municipio.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabroysebadev.proyectoticketdeturno.model;
+
+/**
+ *
+ * @author seb4s
+ */
+public class Municipio {
+    private int id_municipio;
+    private String nombre;
+    
+    public Municipio(){}
+    
+    public Municipio(int id_municipio, String nombre){
+        this.id_municipio = id_municipio;
+        this.nombre = nombre;
+    }
+
+    public int getId_municipio() {
+        return id_municipio;
+    }
+
+    public void setId_municipio(int id_municipio) {
+        this.id_municipio = id_municipio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
